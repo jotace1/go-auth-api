aa572faa26ceea7256a8797a91efcb7855f4a347
João Carlos <joao.s.castro@kavak.com>
2024-02-17 23:43:02 -0300

    feat: remove dbmate migration

--- db/db.go
+++ db/db.go
@@ -3,12 +3,15 @@ package db
 import (
 	"os"
 
+	"github.com/jotace1/simple-authentication/internal/entity"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Init() *gorm.DB {
 	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+
+	db.AutoMigrate(&entity.Account{})
 
 	if err != nil {
 		panic(err)
